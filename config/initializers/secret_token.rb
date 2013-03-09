bcb0e4d86df0a45289f63a47d383e3cecc2b6ca1
juanandrango <andresjuan97@msn.com>
2013-03-09 10:50:41 -0500

    First commit - adding all files and uploading all to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsappsgithub::Application.config.secret_token = 'c562543d15ffd254c80781a289810b7f0f464751601f0023a20551dae9e08bd6d5505d24a625d0b64a5e32d71514b623e3329941687fe1de4a931c391e9dc0e1'
